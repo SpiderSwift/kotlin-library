508f0dd49799c3b2c095219f19fb7c664d1e05eb
SpiderSwift <artem.pishchalov@gmail.com>
2022-01-19 15:47:37 +0000

    Automated build push

---
+++ build/bin/iosX64/releaseFramework/kotlin_library.framework/Headers/kotlin_library.h
@@ -0,0 +1,182 @@
+#import <Foundation/NSArray.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSError.h>
+#import <Foundation/NSObject.h>
+#import <Foundation/NSSet.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSValue.h>
+
+@class Kotlin_libraryGeneric<T>;
+
+@protocol Kotlin_libraryTestCallback;
+
+NS_ASSUME_NONNULL_BEGIN
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wincompatible-property-type"
+#pragma clang diagnostic ignored "-Wnullability"
+
+#pragma push_macro("_Nullable_result")
+#if !__has_feature(nullability_nullable_result)
+#undef _Nullable_result
+#define _Nullable_result _Nullable
+#endif
+
+__attribute__((swift_name("KotlinBase")))
+@interface Kotlin_libraryBase : NSObject
+- (instancetype)init __attribute__((unavailable));
++ (instancetype)new __attribute__((unavailable));
++ (void)initialize __attribute__((objc_requires_super));
+@end;
+
+@interface Kotlin_libraryBase (Kotlin_libraryBaseCopying) <NSCopying>
+@end;
+
+__attribute__((swift_name("KotlinMutableSet")))
+@interface Kotlin_libraryMutableSet<ObjectType> : NSMutableSet<ObjectType>
+@end;
+
+__attribute__((swift_name("KotlinMutableDictionary")))
+@interface Kotlin_libraryMutableDictionary<KeyType, ObjectType> : NSMutableDictionary<KeyType, ObjectType>
+@end;
+
+@interface NSError (NSErrorKotlin_libraryKotlinException)
+@property (readonly) id _Nullable kotlinException;
+@end;
+
+__attribute__((swift_name("KotlinNumber")))
+@interface Kotlin_libraryNumber : NSNumber
+- (instancetype)initWithChar:(char)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+- (instancetype)initWithShort:(short)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+- (instancetype)initWithInt:(int)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+- (instancetype)initWithLong:(long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+- (instancetype)initWithLongLong:(long long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+- (instancetype)initWithFloat:(float)value __attribute__((unavailable));
+- (instancetype)initWithDouble:(double)value __attribute__((unavailable));
+- (instancetype)initWithBool:(BOOL)value __attribute__((unavailable));
+- (instancetype)initWithInteger:(NSInteger)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
++ (instancetype)numberWithChar:(char)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
++ (instancetype)numberWithShort:(short)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
++ (instancetype)numberWithInt:(int)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
++ (instancetype)numberWithLong:(long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
++ (instancetype)numberWithLongLong:(long long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
++ (instancetype)numberWithFloat:(float)value __attribute__((unavailable));
++ (instancetype)numberWithDouble:(double)value __attribute__((unavailable));
++ (instancetype)numberWithBool:(BOOL)value __attribute__((unavailable));
++ (instancetype)numberWithInteger:(NSInteger)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+@end;
+
+__attribute__((swift_name("KotlinByte")))
+@interface Kotlin_libraryByte : Kotlin_libraryNumber
+- (instancetype)initWithChar:(char)value;
++ (instancetype)numberWithChar:(char)value;
+@end;
+
+__attribute__((swift_name("KotlinUByte")))
+@interface Kotlin_libraryUByte : Kotlin_libraryNumber
+- (instancetype)initWithUnsignedChar:(unsigned char)value;
++ (instancetype)numberWithUnsignedChar:(unsigned char)value;
+@end;
+
+__attribute__((swift_name("KotlinShort")))
+@interface Kotlin_libraryShort : Kotlin_libraryNumber
+- (instancetype)initWithShort:(short)value;
++ (instancetype)numberWithShort:(short)value;
+@end;
+
+__attribute__((swift_name("KotlinUShort")))
+@interface Kotlin_libraryUShort : Kotlin_libraryNumber
+- (instancetype)initWithUnsignedShort:(unsigned short)value;
++ (instancetype)numberWithUnsignedShort:(unsigned short)value;
+@end;
+
+__attribute__((swift_name("KotlinInt")))
+@interface Kotlin_libraryInt : Kotlin_libraryNumber
+- (instancetype)initWithInt:(int)value;
++ (instancetype)numberWithInt:(int)value;
+@end;
+
+__attribute__((swift_name("KotlinUInt")))
+@interface Kotlin_libraryUInt : Kotlin_libraryNumber
+- (instancetype)initWithUnsignedInt:(unsigned int)value;
++ (instancetype)numberWithUnsignedInt:(unsigned int)value;
+@end;
+
+__attribute__((swift_name("KotlinLong")))
+@interface Kotlin_libraryLong : Kotlin_libraryNumber
+- (instancetype)initWithLongLong:(long long)value;
++ (instancetype)numberWithLongLong:(long long)value;
+@end;
+
+__attribute__((swift_name("KotlinULong")))
+@interface Kotlin_libraryULong : Kotlin_libraryNumber
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value;
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value;
+@end;
+
+__attribute__((swift_name("KotlinFloat")))
+@interface Kotlin_libraryFloat : Kotlin_libraryNumber
+- (instancetype)initWithFloat:(float)value;
++ (instancetype)numberWithFloat:(float)value;
+@end;
+
+__attribute__((swift_name("KotlinDouble")))
+@interface Kotlin_libraryDouble : Kotlin_libraryNumber
+- (instancetype)initWithDouble:(double)value;
++ (instancetype)numberWithDouble:(double)value;
+@end;
+
+__attribute__((swift_name("KotlinBoolean")))
+@interface Kotlin_libraryBoolean : Kotlin_libraryNumber
+- (instancetype)initWithBool:(BOOL)value;
++ (instancetype)numberWithBool:(BOOL)value;
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Generic")))
+@interface Kotlin_libraryGeneric<T> : Kotlin_libraryBase
+- (instancetype)initWithT:(T _Nullable)t __attribute__((swift_name("init(t:)"))) __attribute__((objc_designated_initializer));
+- (Kotlin_libraryGeneric<T> *)doCopyT:(T _Nullable)t __attribute__((swift_name("doCopy(t:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (T _Nullable)get __attribute__((swift_name("get()")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (void)setT:(T _Nullable)t __attribute__((swift_name("set(t:)")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Test")))
+@interface Kotlin_libraryTest : Kotlin_libraryBase
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (void)delay __attribute__((swift_name("delay()")));
+- (int64_t)durationMinusMinus:(int64_t)minus __attribute__((swift_name("durationMinus(minus:)")));
+- (int64_t)durationPlusPlus:(int64_t)plus __attribute__((swift_name("durationPlus(plus:)")));
+- (void)registerCallback:(id<Kotlin_libraryTestCallback>)callback __attribute__((swift_name("register(callback:)")));
+- (void)trigger __attribute__((swift_name("trigger()")));
+@property (readonly) int32_t integer __attribute__((swift_name("integer")));
+@property (readonly) NSArray<NSString *> *list __attribute__((swift_name("list")));
+@property (readonly) NSDictionary<NSString *, NSString *> *map __attribute__((swift_name("map")));
+@end;
+
+__attribute__((swift_name("TestCallback")))
+@protocol Kotlin_libraryTestCallback
+@required
+- (void)onCallbackMsg:(NSString *)msg __attribute__((swift_name("onCallback(msg:)")));
+@end;
+
+#pragma pop_macro("_Nullable_result")
+#pragma clang diagnostic pop
+NS_ASSUME_NONNULL_END
